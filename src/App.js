6bba6daae6986c3674700538bb12fcbdcfd855a6
Antoine Paris <antoine@MacBook-Pro-de-Antoine-3.local>
2019-06-14 16:33:20 +0200

    add couch db

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import PouchDB from 'pouchdb';
 var db = new PouchDB('my_database');
+var remoteCouch = 'http://Admin:pass@127.0.0.1:5984/test';
+var opts = {live: true};
+db.replicate.to(remoteCouch, opts, console.log);
+db.replicate.from(remoteCouch, opts, console.log);
+
 
 export class App extends React.Component {
 
