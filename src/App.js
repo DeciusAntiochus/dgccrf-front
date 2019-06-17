8f8fbde563be7b034727b5e0802cf0364552ed92
Antoine Paris <antoine@MacBook-Pro-de-Antoine-3.local>
2019-06-17 13:32:47 +0200

    setup couchdb for multi browser

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import PouchDB from 'pouchdb';
-var db = new PouchDB('my_database');
-var remoteCouch = 'http://Admin:pass@127.0.0.1:5984/test';
-var opts = {live: true};
-db.replicate.to(remoteCouch, opts, console.log);
-db.replicate.from(remoteCouch, opts, console.log);
+import {AddDocs} from './AddDocs';
 
-
-export class App extends React.Component {
-
+export class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      text : '',
-      list:[]
-    }
-    db.changes({
-      since: 'now',
-      live: true
-    }).on('change', this.updateTodos.bind(this));
-  }
-
-  updateTodos(){
-    db.allDocs({include_docs: true, descending: true}, (err, doc) => {
-      this.setState({list:doc.rows});
-    });
-  }
-
-  addTodo(text) {
-    var todo = {
-      _id: new Date().toISOString(),
-      title: text,
-      completed: false
+      user: '',
+      validated: false
     };
-    db.put(todo, function callback(err, result) {
-      if (!err) {
-        console.log('Successfully posted a todo!');
-      }
-    });
   }
 
-  getTodos(){
-
-  }
-  
-  render() {
+  render(){
     return (
-      <div>
-        <input onChange = {(e)=>this.setState({text:e.target.value})} /> 
-        <button onClick={()=>this.addTodo(this.state.text)}>Ajouter</button>
-        <button onClick={()=>this.getTodos()}>refresh</button>
+      
+        ! this.state.validated ?
         <div>
-          {JSON.stringify(this.state.list)}
+          <input onChange = {(e)=>this.setState({user:e.target.value})} placeholder="User"/> 
+          <button onClick = {(e)=>this.setState({validated:true})}>Valider</button>
         </div>
-      </div>
-    );
+        :<AddDocs user={this.state.user} />
+    )
   }
 }
-
 
