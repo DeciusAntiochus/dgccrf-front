67557473a12ce12f9e0bdbe31dbd9a4d7fe43834
Alexandra Jerome <alexandra.jerome@student.ecp.fr>
2019-06-17 15:24:09 +0200

    Add prettier and config prettier

--- src/App.js
+++ src/App.js
@@ -4,60 +4,12 @@ import './App.css';
 import PouchDB from 'pouchdb';
 var db = new PouchDB('my_database');
 var remoteCouch = 'http://Admin:pass@127.0.0.1:5984/test';
-var opts = {live: true};
+var opts = { live: true };
 db.replicate.to(remoteCouch, opts, console.log);
 db.replicate.from(remoteCouch, opts, console.log);
 
-
 export class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      text : '',
-      list:[]
-    }
-    db.changes({
-      since: 'now',
-      live: true
-    }).on('change', this.updateTodos.bind(this));
-  }
-
-  updateTodos(){
-    db.allDocs({include_docs: true, descending: true}, (err, doc) => {
-      this.setState({list:doc.rows});
-    });
-  }
-
-  addTodo(text) {
-    var todo = {
-      _id: new Date().toISOString(),
-      title: text,
-      completed: false
-    };
-    db.put(todo, function callback(err, result) {
-      if (!err) {
-        console.log('Successfully posted a todo!');
-      }
-    });
-  }
-
-  getTodos(){
-
-  }
-  
   render() {
-    return (
-      <div>
-        <input onChange = {(e)=>this.setState({text:e.target.value})} /> 
-        <button onClick={()=>this.addTodo(this.state.text)}>Ajouter</button>
-        <button onClick={()=>this.getTodos()}>refresh</button>
-        <div>
-          {JSON.stringify(this.state.list)}
-        </div>
-      </div>
-    );
+    return <div></div>;
   }
 }
-
-
