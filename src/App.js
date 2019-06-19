570dc2aa93214b8cd1454aec0459d8953a2c4236
Antoine Paris <antoine@MacBook-Pro-de-Antoine-3.local>
2019-06-19 11:50:33 +0200

    conflicts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import {AddDocs} from './AddDocs';
-
-export class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      user: '',
-      validated: false
-    };
-  }
-
-  render(){
-    return (
-      
-        ! this.state.validated ?
-        <div>
-          <input onChange = {(e)=>this.setState({user:e.target.value})} placeholder="User"/> 
-          <button onClick = {(e)=>this.setState({validated:true})}>Valider</button>
-        </div>
-        :<AddDocs user={this.state.user} />
-    )
-  }
-}
-
-=======
 import PouchDB from 'pouchdb';
 var db = new PouchDB('my_database');
 var remoteCouch = 'http://Admin:pass@127.0.0.1:5984/test';
@@ -38,4 +12,3 @@ export class App extends React.Component {
     return <div></div>;
   }
 }
->>>>>>> 3be8429e973c5dbfac173014c5140122d6950526
