7dee9ba2945b3a7c54aaf6a546bdc4492480e52d
DeciusAntiochus <awaidaantoine@yahoo.fr>
2019-06-19 10:32:26 +0200

    sql contact

--- src/AddDocs.js
+++ src/AddDocs.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './App.css';
-import PouchDB from 'pouchdb';
-
+import React from "react";
+import "./App.css";
+import PouchDB from "pouchdb";
 
 export class AddDocs extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      text : '',
-      list:[]
-    }
+      text: "",
+      list: []
+    };
 
-    this.db = new PouchDB('my_database');
-    var remoteCouch = 'http://Admin:pass@172.17.64.137:5984/test';
+    this.db = new PouchDB("my_database");
+    var remoteCouch = "http://Admin:password@172.17.64.136:5984/test";
 
-    this.db.changes({
-      since: 'now',
-      live: true
-    }).on('change', this.updateTodos.bind(this));
+    this.db
+      .changes({
+        since: "now",
+        live: true
+      })
+      .on("change", this.updateTodos.bind(this));
 
     var opts = {
-        live: true, 
-        filter: 'filter/by_agent',
-        query_params: { "agent": this.props.user },
+      live: true
+      // filter: "filter/by_agent",
+      // query_params: { agent: this.props.user }
     };
-    this.db.replicate.to(remoteCouch, {live:true}, console.log);
+    this.db.replicate.to(remoteCouch, { live: true }, console.log);
     this.db.replicate.from(remoteCouch, opts, console.log);
     this.updateTodos();
 
@@ -33,14 +33,13 @@ export class AddDocs extends React.Component {
     this.addTodo = this.addTodo.bind(this);
   }
 
-
-  updateTodos(){
-    this.db.allDocs({include_docs: true, descending: true}, (err, doc) => {
-      this.setState({list:doc.rows.map(row=>row.doc)});
+  updateTodos() {
+    this.db.allDocs({ include_docs: true, descending: true }, (err, doc) => {
+      this.setState({ list: doc.rows.map(row => row.doc) });
     });
   }
 
-  addTodo(text,user) {
+  addTodo(text, user) {
     var todo = {
       _id: new Date().toISOString(),
       text: text,
@@ -48,22 +47,24 @@ export class AddDocs extends React.Component {
     };
     this.db.put(todo, function callback(err, result) {
       if (!err) {
-        console.log('Successfully posted a todo!');
+        console.log("Successfully posted a todo!");
       }
     });
   }
-  
+
   render() {
     return (
       <div>
-        <input onChange = {(e)=>this.setState({text:e.target.value})} placeholder="Text"/> 
-        <button onClick={()=>this.addTodo(this.state.text,this.props.user)}>Ajouter</button>
-        <button onClick={()=>this.getTodos()}>refresh</button>
-        <div>
-          {JSON.stringify(this.state.list)}
-        </div>
+        <input
+          onChange={e => this.setState({ text: e.target.value })}
+          placeholder="Text"
+        />
+        <button onClick={() => this.addTodo(this.state.text, this.props.user)}>
+          Ajouter
+        </button>
+        <button onClick={() => this.getTodos()}>refresh</button>
+        <div>{JSON.stringify(this.state.list)}</div>
       </div>
     );
   }
 }
-
