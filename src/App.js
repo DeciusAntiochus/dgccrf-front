3a8a52647932114a736b46b6b1e15f4cea90b593
Alexandra Jerome <alexandra.jerome@student.ecp.fr>
2019-06-21 12:08:41 +0200

    Adapt nav to device width

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Menu from './containers/menu';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import NavBar from './containers/navbar';
-import FileNavigation from './containers/fileNavigation';
 
 // var db = new PouchDB('my_database');
 // var remoteCouch = 'http://Admin:pass@127.0.0.1:5984/test';
--- src/containers/navbar/navbar.container.js
+++ src/containers/navbar/navbar.container.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Menu, Container, Dropdown, Icon } from 'semantic-ui-react';
+import { Menu, Container, Dropdown, Icon, Responsive } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router/esm/react-router';
 
@@ -8,28 +8,40 @@ class NavBarComponent extends React.Component {
   render() {
     return (
       <Menu fixed="top" color="blue" inverted>
-        <Container>
-          <Menu.Item as="a" onClick={() => this.props.history.goBack()}>
-            <Icon name="angle left" />
-            Retour
-          </Menu.Item>
-          <Menu.Menu position="right">
-            <Dropdown item simple text="Menu">
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/mes-dossiers">
-                  <Icon name="file" /> Mes Dossiers
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/entreprises">
-                  <Icon name="building outline" />
-                  Entreprises
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/preferences">
-                  <Icon name="setting"></Icon>Préférences
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+        <Menu.Item as="a" onClick={() => this.props.history.goBack()}>
+          <Icon name="angle left" />
+          Retour
+        </Menu.Item>
+        <Responsive minWidth={600}>
+          <Menu.Menu position="right" icon="labeled">
+            <Menu.Item as={Link} to="/mes-dossiers">
+              <Icon name="file" /> Mes Dossiers
+            </Menu.Item>
+            <Menu.Item as={Link} to="/entreprises">
+              <Icon name="building outline" />
+              Entreprises
+            </Menu.Item>
+            <Menu.Item as={Link} to="/preferences">
+              <Icon name="setting"></Icon>Préférences
+            </Menu.Item>
           </Menu.Menu>
-        </Container>
+        </Responsive>
+        <Responsive maxWidth={600}>
+          <Dropdown item simple text="Menu">
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/mes-dossiers">
+                <Icon name="file" /> Mes Dossiers
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/entreprises">
+                <Icon name="building outline" />
+                Entreprises
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/preferences">
+                <Icon name="setting"></Icon>Préférences
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Responsive>
       </Menu>
     );
   }
