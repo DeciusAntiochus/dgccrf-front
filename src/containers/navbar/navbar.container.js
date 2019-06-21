344aa63c3a5743a7c1f07b4f89cb385610ad8ec4
Alexandra Jerome <alexandra.jerome@student.ecp.fr>
2019-06-21 15:16:20 +0200

    Add name of page

--- src/containers/navbar/navbar.container.js
+++ src/containers/navbar/navbar.container.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Menu, Container, Dropdown, Icon, Responsive } from 'semantic-ui-react';
+import { Menu, Dropdown, Icon, Responsive } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router/esm/react-router';
 
 class NavBarComponent extends React.Component {
   render() {
     return (
-      <Menu fixed="top" color="blue" inverted>
-        <Menu.Item as="a" onClick={() => this.props.history.goBack()}>
-          <Icon name="angle left" />
-          Retour
-        </Menu.Item>
-        <Responsive minWidth={600}>
-          <Menu.Menu position="right" icon="labeled">
-            <Menu.Item as={Link} to="/mes-dossiers">
-              <Icon name="file" /> Mes Dossiers
-            </Menu.Item>
+      <div>
+        <Menu
+          as={Responsive}
+          minWidth={500}
+          fixed="top"
+          color="blue"
+          inverted
+          icon="labeled"
+        >
+          <Menu.Item as="a" onClick={() => this.props.history.goBack()}>
+            <Icon style={{ margin: '0' }} name="angle left" />
+            Retour
+          </Menu.Item>
+          <Menu.Item as={Link} to="/mes-dossiers">
+            <Icon name="file" /> Mes Dossiers
+          </Menu.Item>
+          <Menu.Item style={{ flex: '1', verticalAlign: 'middle' }}>
+            <p style={{ fontSize: '20px' }}>Nom de la page</p>
+          </Menu.Item>
+          <Menu.Menu position="right">
             <Menu.Item as={Link} to="/entreprises">
               <Icon name="building outline" />
               Entreprises
@@ -25,24 +35,39 @@ class NavBarComponent extends React.Component {
               <Icon name="setting"></Icon>Préférences
             </Menu.Item>
           </Menu.Menu>
-        </Responsive>
-        <Responsive maxWidth={600}>
-          <Dropdown item simple text="Menu">
-            <Dropdown.Menu>
-              <Dropdown.Item as={Link} to="/mes-dossiers">
-                <Icon name="file" /> Mes Dossiers
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/entreprises">
-                <Icon name="building outline" />
-                Entreprises
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/preferences">
-                <Icon name="setting"></Icon>Préférences
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Responsive>
-      </Menu>
+        </Menu>
+        <Menu
+          as={Responsive}
+          maxWidth={500}
+          fixed="top"
+          color="blue"
+          inverted
+          size="large"
+        >
+          <Menu.Item as="a" onClick={() => this.props.history.goBack()}>
+            <Icon style={{ margin: '0' }} name="angle left" />
+          </Menu.Item>
+          <Menu.Item style={{ flex: '1' }}>
+            <p style={{ flex: '1', textAlign: 'center' }}>Nom de la page</p>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Dropdown item simple icon="list layout">
+              <Dropdown.Menu>
+                <Dropdown.Item as={Link} to="/mes-dossiers">
+                  <Icon name="file" /> Mes Dossiers
+                </Dropdown.Item>
+                <Dropdown.Item as={Link} to="/entreprises">
+                  <Icon name="building outline" />
+                  Entreprises
+                </Dropdown.Item>
+                <Dropdown.Item as={Link} to="/preferences">
+                  <Icon name="setting"></Icon>Préférences
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Menu>
+        </Menu>
+      </div>
     );
   }
 }
