a2549a4769a88558fc22692394982a3fb64f2465
Antoine Paris <antoine@MacBook-Pro-de-Antoine-3.local>
2019-06-25 14:55:01 +0200

    added trame creation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Menu from './containers/menu';
 import Dossiers from './containers/mes-dossiers';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import MonDossier from './containers/dossier';
-import CreateVisite from './containers/dossier/createVisite';
+import CreateVisite from './containers/dossier/createVisite.container';
+import CreateTrame from './containers/dossier/trameCreation.container';
 import MaVisite from './containers/visite';
+
 
 // var db = new PouchDB('dbdgccrf');
 // var remoteCouch = 'http://Admin:password@172.17.64.136:5984/test';
@@ -30,15 +32,17 @@ export class App extends React.Component {
       <div style={{ overflow: 'hidden', height: '100vh' }}>
         <Router>
           <NavBar />
-
-          <Route exact path="/menu" component={Menu} />
-          <Route exact path="/mes-dossiers" component={Dossiers} />
-          <Route exact path="/dossier/:id" component={MonDossier} />
-          <Route exact path="/visite/:id" component={MaVisite} />
-          <Route exact path="/nouvelle-visite" component={CreateVisite} />
-          {/* <Redirect to="/menu"></Redirect> */}
+          <Switch>
+            <Route exact path="/menu" component={Menu} />
+            <Route exact path="/mes-dossiers" component={Dossiers} />
+            <Route exact path="/dossier/:id" component={MonDossier} />
+            <Route exact path="/visite/:id" component={MaVisite} />
+            <Route exact path="/nouvelle-visite" component={CreateVisite} />
+            <Route exact path="/nouvelle-trame" component={CreateTrame} />
+            <Route component={() => <Redirect to="/menu" />} />
+          </Switch>
         </Router>
-      </div>
+      </div >
     );
   }
 }
