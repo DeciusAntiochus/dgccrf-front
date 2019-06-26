bd448c29e58046b57adc255f5509e951cea327f8
DeciusAntiochus <awaidaantoine@yahoo.fr>
2019-06-26 11:27:30 +0200

    UPDATE: mes dossiers

---
+++ src/containers/mes-dossiers/dossier.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Icon, Grid, Segment, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import './dossier.css';
+
+class Dossier extends React.Component {
+  render() {
+    return (
+      <Grid.Row style={{ padding: '20px' }}>
+        <Segment
+        // to={this.props.link}
+        // icon
+        // basic
+        // fluid
+        // className="menubutton"
+        // color={this.props.color}
+        // size="massive"
+        >
+          <Link to={this.props.link}>
+            <Grid>
+              <Grid.Row verticalAlign="middle" style={{ padding: 0 }}>
+                <Grid.Column width={3} style={{ backgroundColor: '#f2f2f2' }}>
+                  <Icon name="folder" color="yellow" size="huge"></Icon>
+                </Grid.Column>
+                <Grid.Column width={8} textAlign="left">
+                  <Header as="h2">{this.props.name}</Header>
+                </Grid.Column>
+                <Grid.Column width={5}>
+                  <Header as="h3">Salut</Header>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Link>
+        </Segment>
+      </Grid.Row>
+    );
+  }
+}
+
+Dossier.propTypes = {
+  link: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default Dossier;
--- src/containers/mes-dossiers/dossiers.container.js
+++ src/containers/mes-dossiers/dossiers.container.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import MenuButton from '../../components/menuButton.component';
+
+import Dossier from './dossier';
 
 export default class DossierComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      taskList: [{
-        name: "TN 20131441",
-        id: "19920a3dd"
-      },
-      {
-        name: "TR 20145323",
-        id: "127226rf3"
-      }]
-    }
+      taskList: [
+        {
+          name: 'TN 20131441',
+          id: '19920a3dd'
+        },
+        {
+          name: 'TR 20145323',
+          id: '127226rf3'
+        }
+      ]
+    };
   }
   render() {
     return (
-      <Grid
-        textAlign="center"
-        style={{ height: '100vh' }}
-        verticalAlign="top"
-      >
-        <Grid.Column style={{ maxWidth: "90vw", margin: "1em" }}>
-
+      <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="top">
+        <Grid.Column style={{ maxWidth: '90vw', margin: '1em' }}>
           {this.state.taskList.map(task => (
-            <MenuButton key={task.id} name={task.name} link={"dossier/" + task.id} color="white" />
+            <Dossier
+              icon="folder"
+              iconcolor="yellow"
+              key={task.id}
+              name={task.name}
+              link={'dossier/' + task.id}
+              color="white"
+            />
           ))}
-
         </Grid.Column>
-      </Grid >
-
+      </Grid>
     );
   }
 }
