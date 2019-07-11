4443c9fa9d616c149a89610824756e669a6f18f3
Alexandra Jerome <alexandra.jerome@student.ecp.fr>
2019-07-11 10:56:23 +0200

    change service bug

--- src/services/subservices/cpf.service.js
+++ src/services/subservices/cpf.service.js
@@ -5,17 +5,21 @@ import replicateFromSQL from '../replicationHandler';
 
 PouchDB.plugin(PouchDBFind);
 class CpfService {
-    constructor() {
-        let pouchDbUrl = config.couchDb.url_cpf;
-        this.db = new PouchDB('cpf');
-        var opts = {
-            batch_size: 1000,
-            live: true,
-            retry: true,
-        };
-        this.replication = replicateFromSQL(this.db, config.backend.base_url + '/fulldata/cpf/', 'cpf_date');
-        this.resetDb = () => undefined; // no need to reset activite
-
+  constructor() {
+    let pouchDbUrl = config.couchDb.url_cpf;
+    this.db = new PouchDB('cpf');
+    var opts = {
+      batch_size: 1000,
+      live: true,
+      retry: true
+    };
+    this.replication = replicateFromSQL(
+      this.db,
+      config.backend.base_url + '/fulldata/cpf/',
+      'cpf_date'
+    );
+    this.resetDb = () => undefined; // no need to reset activite
+  }
   //getAllDocsOfTheDB
   getAllDocs() {
     return this.db
