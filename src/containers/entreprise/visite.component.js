f472f8c99359a3928c5992b7aa40ed9122951135
Antoine Paris <antoineparis77@gmail.com>
2019-07-16 18:57:08 +0200

    rework visites and controles

--- src/containers/entreprise/visite.component.js
+++ src/containers/entreprise/visite.component.js
@@ -1,93 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Grid, Segment, Responsive, Icon, Header } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { DisplayAction } from '../dossier/visiteActions';
 
 export default class Visite extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showControles: false
+        }
+    }
     render() {
         const { visite } = this.props;
         return (
-            <Grid.Row style={{ padding: 10, overflow: 'auto' }}>
-                <Segment
-                    style={{ width: '100%' }}
-                // to={this.props.link}
-                // icon
-                // basic
-                // fluid
-                // className="menubutton"
-                // color={this.props.color}
-                // size="massive"
-                >
-                    <Grid>
-                        <Grid.Row
-                            verticalAlign="middle"
-                            style={{
-                                padding: 0,
-                                //backgroundColor: !visite.visiteData.new_visite && 'grey'
-                            }}
-                        >
-                            <Grid.Column
-                                width={3}
+            <Fragment style={{ margin: "0.5em" }}>
+                <Grid.Row style={{ padding: 10, overflow: 'auto' }}>
+                    <Segment
+                        style={{ width: '100%' }}
+                    >
+                        <Grid>
+                            <Grid.Row
+                                verticalAlign="middle"
                                 style={{
-                                    backgroundColor: '#f2f2f2',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    height: '100%'
+                                    padding: 0,
+                                    //backgroundColor: !visite.visiteData.new_visite && 'grey'
                                 }}
                             >
-                                <Responsive minWidth={600}>
-                                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                                        <Icon name="search" color="grey" size="large"></Icon>
-                                        <div
-                                            style={{
-                                                borderRadius: '50%',
+                                <Grid.Column
+                                    width={3}
+                                    onClick={() => this.setState({ showControles: !this.state.showControles })}
+                                    style={{
+                                        backgroundColor: '#f2f2f2',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        minHeight: '30px'
+                                    }}
+                                >
+                                    <Responsive minWidth={600}>
+                                        <div style={{ display: 'flex', flexDirection: 'row', textAlign: 'center' }}>
+                                            <Icon name="search" color="grey" size="large"></Icon>
+                                            <div
+                                                style={{
+                                                    borderRadius: '50%',
 
-                                                width: '20px',
-                                                height: '20px',
+                                                    width: '20px',
+                                                    height: '20px',
 
-                                                background: 'red',
-                                                color: 'white'
-                                            }}
-                                        >
-                                            {visite.controles.length}
+                                                    background: 'red',
+                                                    color: 'white'
+                                                }}
+                                            >
+                                                {visite.controles.length}
+                                            </div>
                                         </div>
-                                    </div>
-                                </Responsive>
-                                <Responsive maxWidth={599}>
-                                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                                        <Icon
-                                            name="search"
-                                            color="grey"
-                                            size="small"
-                                            style={{ fontSize: '1em' }}
-                                        ></Icon>
-                                        <div
-                                            style={{
-                                                borderRadius: '50%',
+                                    </Responsive>
+                                    <Responsive maxWidth={599}>
+                                        <div style={{ display: 'flex', flexDirection: 'row', textAlign: 'center' }}>
+                                            <Icon
+                                                name="search"
+                                                color="grey"
+                                                size="small"
+                                                style={{ fontSize: '1em' }}
+                                            ></Icon>
+                                            <div
+                                                style={{
+                                                    borderRadius: '50%',
 
-                                                width: '20px',
-                                                height: '20px',
+                                                    width: '20px',
+                                                    height: '20px',
 
-                                                background: 'red',
-                                                color: 'white'
-                                            }}
-                                        >
-                                            {visite.controles.length}
+                                                    background: 'red',
+                                                    color: 'white'
+                                                }}
+                                            >
+                                                {visite.controles.length}
+                                            </div>
                                         </div>
-                                    </div>
-                                </Responsive>
-                            </Grid.Column>
-                            <Grid.Column width={7} textAlign="left">
-                                <Grid.Row>
+                                    </Responsive>
+                                </Grid.Column>
+                                <Grid.Column width={7} textAlign="left">
                                     <Header as="h3" style={{ fontWeight: 'normal' }}>
                                         {visite.VISITE_AGENT_LIBELLE}
                                     </Header>
-                                </Grid.Row>
-                            </Grid.Column>
-                            <Grid.Column width={6}>
-                                <Grid.Row>
+                                </Grid.Column>
+                                <Grid.Column width={6}>
                                     <Header
                                         as="h3"
                                         style={{ fontWeight: 'normal', fontStyle: 'italic' }}
@@ -96,12 +94,15 @@ export default class Visite extends Component {
                                             visite.VISITE_DATE_INTERVENTION
                                         ).format('DD/MM/YYYY')}
                                     </Header>
-                                </Grid.Row>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Segment>
-            </Grid.Row>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                </Grid.Row>
+
+
+                {this.state.showControles && <DisplayAction controles={visite.controles} />}
+            </Fragment>
         );
     }
 }
